c8f0a824b136369514752bdcafe7380c60fb3469
EgawaShunta <egasyun19@gmail.com>
2021-12-01 18:20:57 +0900

    とりあえずコミット

--- intern_project/cookapp/static/javascript/base.js
+++ intern_project/cookapp/static/javascript/base.js
@@ -5,36 +5,44 @@ const button3 = document.getElementById('button3')
 const button4 = document.getElementById('button4')
 
 // homeボタンがオレンジ
-button1.addEventListener('click') = function() {  
+button1.addEventListener('click', function() {
+
     button1.src = '../svg/home_orange.svg'
     button2.src = '../svg/friends.svg'
     button3.src = '../svg/submission.svg'
     button4.src = '../svg/frineds_add_before.svg'
-};
+    
+})   
 
 // friendsボタンがオレンジ
-button2.addEventListener('click') = function() {  
+button2.addEventListener('click', function(){
+
     button1.src = '../svg/home.svg'
     button2.src = '../svg/friends_orange.svg'
     button3.src = '../svg/submission.svg'
     button4.src = '../svg/frineds_add_before.svg'
-};
+
+});
 
 // submiisonボタンがオレンジ
-button2.addEventListener('click') = function() {  
+button2.addEventListener('click', function(){
+
     button1.src = '../svg/home.svg'
     button2.src = '../svg/friends.svg'
     button3.src = '../svg/submission_orange.svg'
     button4.src = '../svg/frineds_add_before.svg'
-};
+
+});  
 
 // friends追加ボタンがオレンジ
-button2.addEventListener('click') = function() {  
+button2.addEventListener('click', function(){
+
     button1.src = '../svg/home.svg'
     button2.src = '../svg/friends.svg'
     button3.src = '../svg/submission.svg'
     button4.src = '../svg/frineds_add_before_orange.svg'
-};
+    
+});
 
 
 
--- intern_project/static/js/my_content.js
+++ intern_project/static/js/my_content.js
@@ -4,3 +4,21 @@ window.addEventListener('DOMContentLoaded', function(){
     console.log('hello world');
     });
 });
+
+let under = document.getElementById('under')
+let over = document.getElementById('over')
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    console.log(under)
+    console.log(over)
+    console.log(document.getElementById('delete'))
+
+    document.getElementById('delete').addEventListener('click', function() {
+        
+        console.log('成功')
+        under.classList.toggle('under')
+        over.classList.toggle('over')
+        
+    }); 
+});
