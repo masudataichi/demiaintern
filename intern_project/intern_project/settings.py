64267a04ec63502751d8003dd1aad60ee3a41dd3
DeMiAintern <taichi130108@gmail.com>
2021-12-01 23:57:44 +0900

    コメントアウト

--- intern_project/intern_project/settings.py
+++ intern_project/intern_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 AUTH_USER_MODEL = 'cookapp.User'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
