597b3e7eb90d039c2010d30efa5edeb9dadc4039
EgawaShunta <egasyun19@gmail.com>
2021-12-01 19:02:36 +0900

    とりあえずコミット第三弾

--- intern_project/cookapp/models.py
+++ intern_project/cookapp/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     username = models.CharField(
         verbose_name='名前(20文字まで)',
         max_length=20,
-        unique=False,
+        unique=True,
         validators=[username_validator],
         error_messages={
             'unique':("A user with that username already exists."),
