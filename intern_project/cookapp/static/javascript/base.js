11352b61e579a559471ad48c06a5e33c032906fd
EgawaShunta <egasyun19@gmail.com>
2021-12-04 16:53:35 +0900

    まだ投稿していないときの見た目を作ったお

--- intern_project/cookapp/static/javascript/base.js
+++ intern_project/cookapp/static/javascript/base.js
@@ -5,45 +5,6 @@ const button2 = document.getElementById('button2')
 const button3 = document.getElementById('button3')
 const button4 = document.getElementById('button4')
 
-// // homeボタンがオレンジ
-// button1.addEventListener('click', function() {
-
-//     button1.src = '../svg/home_orange.svg'
-//     button2.src = '../svg/friends.svg'
-//     button3.src = '../svg/submission.svg'
-//     button4.src = '../svg/frineds_add_before.svg'
-    
-// })   
-
-// // friendsボタンがオレンジ
-// button2.addEventListener('click', function(){
-
-//     button1.src = '../svg/home.svg'
-//     button2.src = '../svg/friends_orange.svg'
-//     button3.src = '../svg/submission.svg'
-//     button4.src = '../svg/frineds_add_before.svg'
-
-// });
-
-// // submiisonボタンがオレンジ
-// button2.addEventListener('click', function(){
-
-//     button1.src = '../svg/home.svg'
-//     button2.src = '../svg/friends.svg'
-//     button3.src = '../svg/submission_orange.svg'
-//     button4.src = '../svg/frineds_add_before.svg'
-
-// });  
-
-// // friends追加ボタンがオレンジ
-// button2.addEventListener('click', function(){
-
-//     button1.src = '../svg/home.svg'
-//     button2.src = '../svg/friends.svg'
-//     button3.src = '../svg/submission.svg'
-//     button4.src = '../svg/frineds_add_before_orange.svg'
-    
-// });
 
 let current_url = location.pathname.split('/').pop()
 console.log(current_url)
