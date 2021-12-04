e3cfb380ad1146ab942ee698abe7ab91b361bd30
Usukichikun <usuki.rintaro@gmail.com>
2021-12-04 23:55:30 +0900

    unnko

--- intern_project/static/js/my_content.js
+++ intern_project/static/js/my_content.js
@@ -8,11 +8,21 @@ window.addEventListener('DOMContentLoaded', function(){
         this.classList.toggle("active");
     });
 });
-//第二次スレッドの表示
-window.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('unclicked').addEventListener('click', function(){
-        document.getElementById('doc').classList.toggle('active');
-        document.getElementById('img').classList.toggle('active');
-        document.getElementById('clicked').classList.toggle('active');
-    });
-});
+//第二次スレッド
+const unclick = document.getElementsByClassName('reply-trigger');
+for (var i = 0; i < unclick.length; i++){
+    unclick[i].addEventListener('click', function(){
+        const doc = document.getElementsByClassName('unclicked-doc');
+        for (var i = 0; i < doc.length; i++){
+            doc[i].classList.toggle('active');
+        }
+        const image = document.getElementsByClassName('form-image3');
+        for (var i = 0; i < image.length; i++){
+            image[i].classList.toggle('active');
+        }
+        const form = document.getElementsByClassName('replyform');
+        for (var i = 0; i < form.length; i++){
+            form[i].classList.toggle('active');
+        };
+     });
+};
